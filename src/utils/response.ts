6e52580fd5f3401a665055f30f86e6cd7653c238
wangding <wangding@shwoody.com>
2025-10-16 15:05:56 +0800

    feat: 更新请求头

--- src/utils/response.ts
+++ src/utils/response.ts
@@ -85,7 +85,7 @@ export function corsPreflightResponse(): Response {
     headers: {
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization, X-Content-Hash, X-File-Name',
       'Access-Control-Max-Age': '86400',
     },
   });
